a9ab7fbbc4bc65a8c0ee4cb23c18b04e155d9da4
Pranavsinghs <120789096+Pranavsinghs@users.noreply.github.com>
2023-03-29 16:54:47 +0530

    Add files via upload

---
+++ basketball.js
@@ -0,0 +1,41 @@
+let countEl=document.getElementById("count-el")
+let btnEl=document.getElementById("btn-el")
+let count1El=document.getElementById("count1-el")
+let scoreEl=document.getElementById("score-el")
+
+let count=0;
+let count1=0;
+
+
+
+function one1() {
+    count +=1;
+    countEl.textContent=count;
+}
+
+function two2() {
+    count +=2;
+    countEl.textContent=count;
+}
+
+function three3() {
+    count +=3;
+    countEl.textContent=count;
+}
+
+
+
+function one11() {
+    count1 +=1;
+    count1El.textContent=count1;
+}
+
+function two22() {
+    count1 +=2;
+    count1El.textContent=count1;
+}
+
+function three33() {
+    count1 +=3;
+    count1El.textContent=count1;
+}
